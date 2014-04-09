c4d5572f7177ff4b8bafac1ae54e1593354e0a93
Max Holder <mxhold@gmail.com>
2014-04-08 22:05:26 -0400

    Prevent install script from symlinking git repo

--- install.rb
+++ install.rb
@@ -2,7 +2,7 @@
 # Symlink files in current directory to ~
 
 base_path = Dir.pwd
-Dir.glob("#{base_path}/.*") do |filename|
+Dir.glob("#{base_path}/.*").reject { |path| path[/\.git\z/] }.each do |filename|
   `ln -s #{filename} ~/#{File.basename(filename)}`
 end
 
